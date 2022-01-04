73e822602dfc04c5dfff0562a52c7478443c9c2b
Zane (She/They) <55933131+PythonCoderUnicorn@users.noreply.github.com>
2022-01-04 16:19:09 -0700

    days 5 destroy a square

---
+++ day5.R
@@ -0,0 +1,33 @@
+
+# ======== day 5
+
+library(tidyverse)
+library(showtext)
+library(paletteer)
+library(ggtext)
+library(magick)
+library(imager)
+library(ggfx)
+
+font_add_google('Poppins','Poppins')
+showtext_auto()
+
+
+df = data.frame(
+  x = rnorm(115, mean = 6, sd=3),
+  y= rnorm(115, mean = 6, sd=3)
+)
+
+df %>% 
+  ggplot(
+    aes(x= x,
+        y=y)
+  )+
+  geom_polygon(fill='purple')+
+  theme_void()+
+  theme(legend.position = "none",
+        panel.background = element_rect(fill = "black"))
+
+
+
+
